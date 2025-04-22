b5290540033331128936f09e7fe6644ce0c5f47e
zhichao-aws <zhichaog@amazon.com>
2025-04-22 10:22:43 +0800

    prune, batch size
    Signed-off-by: zhichao-aws <zhichaog@amazon.com>

--- examples/deploy_on_sagemaker/handler/neural_sparse_handler.py
+++ examples/deploy_on_sagemaker/handler/neural_sparse_handler.py
@@ -13,50 +13,59 @@ import transformers
 from ts.torch_handler.base_handler import BaseHandler
 
 model_id = "opensearch-project/opensearch-neural-sparse-encoding-doc-v2-distill"
+prune_ratio = 0.0
+max_bs = 50
+
 
 class SparseEncodingModelHandler(BaseHandler):
     class SparseModel(torch.nn.Module):
         @staticmethod
         def from_pretrained(path):
             return SparseEncodingModelHandler.SparseModel(path)
-        
+
         def __init__(self, model_id):
             super().__init__()
             self.backbone = transformers.AutoModelForMaskedLM.from_pretrained(model_id)
             self.special_token_ids = []
-            
-        def set_special_token_ids(self,special_token_ids):
+
+        def set_special_token_ids(self, special_token_ids):
             self.special_token_ids = special_token_ids
-            
+
         def forward(self, **kwargs):
             output = self.backbone(**kwargs)[0]
-            values, _ = torch.max(output*kwargs.get("attention_mask").unsqueeze(-1), dim=1)
+            values, _ = torch.max(
+                output * kwargs.get("attention_mask").unsqueeze(-1), dim=1
+            )
             values = torch.log(1 + torch.relu(values))
-            values[:,self.special_token_ids] = 0
-            return values
-    
+            values[:, self.special_token_ids] = 0
+            max_values = values.max(dim=-1)[0].unsqueeze(1) * prune_ratio
+            return values * (values > max_values)
 
     class SparsePostProcessor(object):
-        def __init__(self,tokenizer):
+        def __init__(self, tokenizer):
             self.tokenizer = tokenizer
             self.id_to_token = ["" for i in range(tokenizer.vocab_size)]
             for token, _id in tokenizer.vocab.items():
                 self.id_to_token[_id] = token
-            
+
         def __call__(self, sparse_vector):
-            sample_indices,token_indices=torch.nonzero(sparse_vector,as_tuple=True)
-            non_zero_values = sparse_vector[(sample_indices,token_indices)].tolist()
-            number_of_tokens_for_each_sample = torch.bincount(sample_indices).cpu().tolist()
+            sample_indices, token_indices = torch.nonzero(sparse_vector, as_tuple=True)
+            non_zero_values = sparse_vector[(sample_indices, token_indices)].tolist()
+            number_of_tokens_for_each_sample = (
+                torch.bincount(sample_indices).cpu().tolist()
+            )
             tokens = [self.id_to_token[_id] for _id in token_indices.tolist()]
 
             output = []
-            end_idxs = list(itertools.accumulate([0]+number_of_tokens_for_each_sample))
-            for i in range(len(end_idxs)-1):
-                token_strings = tokens[end_idxs[i]:end_idxs[i+1]]
-                weights = non_zero_values[end_idxs[i]:end_idxs[i+1]]
+            end_idxs = list(
+                itertools.accumulate([0] + number_of_tokens_for_each_sample)
+            )
+            for i in range(len(end_idxs) - 1):
+                token_strings = tokens[end_idxs[i] : end_idxs[i + 1]]
+                weights = non_zero_values[end_idxs[i] : end_idxs[i + 1]]
                 output.append(dict(zip(token_strings, weights)))
             return output
-    
+
     def __init__(self):
         super().__init__()
         self.special_token_ids = None
@@ -67,17 +76,26 @@ class SparseEncodingModelHandler(BaseHandler):
     def initialize(self, context):
         self.manifest = context.manifest
         properties = context.system_properties
-        
+
         # load model and tokenizer
-        self.device = torch.device("cuda:" + str(properties.get("gpu_id")) if torch.cuda.is_available() else "cpu")
+        self.device = torch.device(
+            "cuda:" + str(properties.get("gpu_id"))
+            if torch.cuda.is_available()
+            else "cpu"
+        )
         self.model = SparseEncodingModelHandler.SparseModel.from_pretrained(model_id)
         self.model.to(self.device)
         self.tokenizer = transformers.AutoTokenizer.from_pretrained(model_id)
-        
-        self.post_processor = SparseEncodingModelHandler.SparsePostProcessor(tokenizer=self.tokenizer)
-        self.special_token_ids = [self.tokenizer.vocab[token] for token in self.tokenizer.special_tokens_map.values()]
+
+        self.post_processor = SparseEncodingModelHandler.SparsePostProcessor(
+            tokenizer=self.tokenizer
+        )
+        self.special_token_ids = [
+            self.tokenizer.vocab[token]
+            for token in self.tokenizer.special_tokens_map.values()
+        ]
         self.model.set_special_token_ids(self.special_token_ids)
-        
+
         self.initialized = True
 
     def preprocess(self, requests):
@@ -88,7 +106,7 @@ class SparseEncodingModelHandler(BaseHandler):
 
             request_body = request.get("body")
             if isinstance(request_body, bytearray):
-                request_body = request_body.decode('utf-8')
+                request_body = request_body.decode("utf-8")
                 request_body = json.loads((request_body))
 
             if isinstance(request_body, list):
@@ -97,30 +115,51 @@ class SparseEncodingModelHandler(BaseHandler):
             else:
                 inputSentence.append(request_body)
                 batch_idx.append(1)
-        input_data = self.tokenizer(inputSentence, padding=True, truncation=True, max_length=512, 
-                                    return_tensors='pt', return_attention_mask=True, return_token_type_ids=False)
+        input_data = self.tokenizer(
+            inputSentence,
+            padding=True,
+            truncation=True,
+            max_length=512,
+            return_tensors="pt",
+            return_attention_mask=True,
+            return_token_type_ids=False,
+        )
 
         input_data = input_data.to(self.device)
         return {"input": input_data, "batch_l": batch_idx}
 
     def inference(self, data, *args, **kwargs):
         batch_idx = data["batch_l"]
-        data = data["input"]
-        with torch.cuda.amp.autocast(),torch.no_grad():
-            output = self.model(**data)
+        data_input = data["input"]
 
+        total_samples = data_input["input_ids"].shape[0]
+        outputs = []
+
+        for start_idx in range(0, total_samples, max_bs):
+            end_idx = min(start_idx + max_bs, total_samples)
+
+            batch_data = {
+                "input_ids": data_input["input_ids"][start_idx:end_idx],
+                "attention_mask": data_input["attention_mask"][start_idx:end_idx],
+            }
+
+            with torch.cuda.amp.autocast(), torch.no_grad():
+                output = self.model(**batch_data)
+                outputs.append(output)
+
+        output = torch.cat(outputs, dim=0)
         return {"pred": output, "batch_l": batch_idx}
 
     def postprocess(self, prediction):
         batch_idx = prediction["batch_l"]
         output = prediction["pred"]
         output = self.post_processor(output)
-        
+
         # return the inference results to each request according to batch size
         outputs = []
         index = 0
         for b in batch_idx:
-            outputs.append(output[index:index + b])
+            outputs.append(output[index : index + b])
             index += b
         return outputs
 
